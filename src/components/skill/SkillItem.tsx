584a6ed2076a729c6ab448141df4232017f15361
Sam Shiles <shilessam@gmail.com>
2024-11-25 00:38:30 +0000

    adjust line height of skills items

--- src/components/skill/SkillItem.tsx
+++ src/components/skill/SkillItem.tsx
@@ -5,7 +5,7 @@ export default function SkillItem ({ name, icon }: Readonly<Skill>) {
       <div className="w-full">
       <div className="flex items-center gap-3 print:gap-1 text-gray-300">
         <div className="w-5 h-5 flex items-center justify-center">{icon}</div>
-        <span className="text-sm leading-none">{name}</span>
+        <span className="text-sm">{name}</span>
       </div>
     </div>
   );
